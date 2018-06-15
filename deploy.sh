933028fd4117563a37fa57a0ebf07efe9264ed2f
giovanni.barros <giovanni.barros@inlocomedia.com>
2018-06-15 18:55:17 -0300

    update deploy and configmap

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
 alias k=kubectl
 
-k apply -f logstash-claim0-persistentvolumeclaim.yaml
-k apply -f logstash-claim1-persistentvolumeclaim.yaml
+k apply -f logging-configmap
+
+k apply -f elasticsearch-claim0-persistentvolumeclaim.yaml
+k apply -f elasticsearch-claim1-persistentvolumeclaim.yaml
+k apply -f elasticsearch-service.yaml
+k apply -f elasticsearch-deployment.yaml
+
+k apply -f kibana-claim0-persistentvolumeclaim.yaml
+k apply -f kibana-claim1-persistentvolumeclaim.yaml
+k apply -f kibana-service.yaml
+k apply -f kibana-deployment.yaml
+
 k apply -f logstash-service.yaml
 k apply -f logstash-deployment.yaml
